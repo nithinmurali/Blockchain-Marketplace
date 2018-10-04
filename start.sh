9ebd0964baf8d5919fba36c5062b1268c8d0edda
nithinmurali <imnmfotmal@gmail.com>
2018-10-04 15:05:21 +0530

    channel creation and joining

---
+++ start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Exit on first error, print all commands.
+set -ev
+
+# don't rewrite paths for Windows Git Bash users
+export MSYS_NO_PATHCONV=1
+
+docker-compose -f docker-compose.yaml down
+
+docker-compose -f docker-compose.yaml up -d
+
+# wait for Hyperledger Fabric to start
+# incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
+export FABRIC_START_TIMEOUT=30
+#echo ${FABRIC_START_TIMEOUT}
+sleep ${FABRIC_START_TIMEOUT}
+
+# Create the channel
+docker exec -e "CORE_PEER_ID=cli1" -e "CORE_PEER_LOCALMSPID=SellerMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/seller.om.com/users/Admin@seller.om.com/msp" cli1 peer channel create -o orderer.om.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
+
+sleep 5
+
+# # Join peer0.org1.example.com to the channel.
+docker exec -e "CORE_PEER_ID=cli1" -e "CORE_PEER_LOCALMSPID=SellerMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/seller.om.com/users/Admin@seller.om.com/msp" -e "CORE_PEER_ADDRESS=peer0.seller.om.com:7051" cli1 peer channel join -b mychannel.block
+
+docker exec -e "CORE_PEER_ID=cli1" -e "CORE_PEER_LOCALMSPID=BuyerMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/buyer.om.com/users/Admin@buyer.om.com/msp" -e "CORE_PEER_ADDRESS=peer0.buyer.om.com:7051" cli1 peer channel join -b mychannel.block
+
+# docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b mychannel.block
