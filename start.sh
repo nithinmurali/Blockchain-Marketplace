68cdbbc5ca1dc3c2113c1859ca67e4085a59c495
nithinmurali <imnmfotmal@gmail.com>
2018-10-05 11:00:22 +0530

    functions working

--- start.sh
+++ start.sh
@@ -19,7 +19,7 @@ sleep ${FABRIC_START_TIMEOUT}
 # Create the channel
 docker exec -e "CORE_PEER_ID=cli1" -e "CORE_PEER_LOCALMSPID=SellerMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/seller.om.com/users/Admin@seller.om.com/msp" cli1 peer channel create -o orderer.om.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
 
-sleep 15
+sleep 30
 
 # # Join peers
 docker exec -e "CORE_PEER_ID=cli1" -e "CORE_PEER_LOCALMSPID=SellerMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/seller.om.com/users/Admin@seller.om.com/msp" -e "CORE_PEER_ADDRESS=peer0.seller.om.com:7051" cli1 peer channel join -b mychannel.block
@@ -28,16 +28,26 @@ docker exec -e "CORE_PEER_ID=cli1" -e "CORE_PEER_LOCALMSPID=BuyerMSP" -e "CORE_P
 
 docker exec -e "CORE_PEER_ID=cli1" -e "CORE_PEER_LOCALMSPID=MarketplaceMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/marketplace.om.com/users/Admin@marketplace.om.com/msp" -e "CORE_PEER_ADDRESS=peer0.marketplace.om.com:7051" cli1 peer channel join -b mychannel.block
 
-docker exec -e "CORE_PEER_ID=cli1" -e "CORE_PEER_LOCALMSPID=MarketplaceMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/marketplace.om.com/users/Admin@marketplace.om.com/msp" -e "CORE_PEER_ADDRESS=peer0.marketplace.om.com:7051" cli1 peer channel join -b mychannel.block
+# docker exec -e "CORE_PEER_ID=cli1" -e "CORE_PEER_LOCALMSPID=MarketplaceMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/marketplace.om.com/users/Admin@marketplace.om.com/msp" -e "CORE_PEER_ADDRESS=peer0.marketplace.om.com:7051" cli1 peer channel join -b mychannel.block
 
-sleep 10
+sleep 20
 
-CODE_VERSION=5.19
+CODE_VERSION=1.0
 
 # deploy the code
 docker exec -e "CORE_PEER_ID=cli1" -e "CORE_PEER_LOCALMSPID=MarketplaceMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/marketplace.om.com/users/Admin@marketplace.om.com/msp" -e "CORE_PEER_ADDRESS=peer0.marketplace.om.com:7051" cli1 peer  chaincode install -n veefin-network -v $CODE_VERSION -l golang -p github.com/OM
+
+docker exec -e "CORE_PEER_ID=cli1" -e "CORE_PEER_LOCALMSPID=SellerMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/seller.om.com/users/Admin@seller.om.com/msp" -e "CORE_PEER_ADDRESS=peer0.seller.om.com:7051" cli1 peer  chaincode install -n veefin-network -v $CODE_VERSION -l golang -p github.com/OM
+
+docker exec -e "CORE_PEER_ID=cli1" -e "CORE_PEER_LOCALMSPID=BuyerMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/buyer.om.com/users/Admin@buyer.om.com/msp" -e "CORE_PEER_ADDRESS=peer0.buyer.om.com:7051" cli1 peer  chaincode install -n veefin-network -v $CODE_VERSION -l golang -p github.com/OM
 
 sleep 5
 
 # instantiate the code
 docker exec -e "CORE_PEER_ID=cli1" -e "CORE_PEER_LOCALMSPID=MarketplaceMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/marketplace.om.com/users/Admin@marketplace.om.com/msp" -e "CORE_PEER_ADDRESS=peer0.marketplace.om.com:7051" cli1 peer  chaincode instantiate -o orderer.om.com:7050 -C mychannel -n veefin-network -l golang -v $CODE_VERSION -c '{"Args":[""]}' -P "OR ('SellerMSP.member', 'BuyerMSP.member')"
+
+sleep 3
+
+docker exec -e "CORE_PEER_LOCALMSPID=SellerMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/seller.om.com/users/Admin@seller.om.com/msp" -e "CORE_PEER_ADDRESS=peer0.seller.om.com:7051" -e "CORE_PEER_ID=cli1" cli1 peer chaincode invoke -o orderer.om.com:7050 -C mychannel -n veefin-network -c '{"function":"createItem","Args":["ITEM0", "10", "100", "Submitted"]}'
+
+docker exec -e "CORE_PEER_LOCALMSPID=SellerMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/seller.om.com/users/Admin@seller.om.com/msp" -e "CORE_PEER_ADDRESS=peer0.seller.om.com:7051" -e "CORE_PEER_ID=cli1" cli1 peer chaincode invoke -o orderer.om.com:7050 -C mychannel -n veefin-network -c '{"function":"queryAllItems","Args":[""]}'
